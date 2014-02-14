38ec4713c12bf5bc6245a2281c7c7e621182cae4
Vova Starikh <vovastarikh@gmail.com>
2014-02-15 01:03:05 +0200

    add module 'SimpleTransform' and enable NEON instructions

--- modules/SimpleTransform/src/CSimpleTransform.h
+++ modules/SimpleTransform/src/CSimpleTransform.h
@@ -12,22 +12,22 @@
 
 #pragma once
 
-class CVector4f
+#include "module/CModule"
+#include "math/CMatrix4f"
+
+class CSimpleTransform : public CModule
 {
 public:
-	CVector4f() {
-		v[0] = v[1] = v[2] = v[3] = 0.0f;
-	}
-	CVector4f(const float x, const float y, const float z, const float w) {
-		v[0] = x; v[1] = y; v[2] = z; v[3] = w;
-	}
-	CVector4f(const float* array) {
-		v[0] = array[0]; v[1] = array[1]; v[2] = array[2]; v[3] = array[3];
-	}
+	CSimpleTransform();
+	~CSimpleTransform();
 
-	void operator +=(const CVector4f& other) { v[0] = other.v[0]; v[1] = other.v[1]; v[2] = other.v[2]; v[3] = other.v[3]; }
-	void operator +=(const float val) { v[0] = val; v[1] = val; v[2] = val; v[3] = val; }
+	virtual bool init(const mongo::BSONObj& initObject);
 
-public:
-	float v[4] __attribute__((aligned(16)));
+private:
+	CMatrix4f m_matrix;
+
+protected:
+
+// notify
+	virtual void recievedData(const mongo::BSONObj& data);
 };
