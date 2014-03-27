a008dbf320d153f467afd6ca417f3e9455276349
Alex Vrubel <alex.vrubel@gmail.com>
2014-03-27 19:53:25 +0400

    register and call callback

--- libraries/dotnet-sdk/src/CDotNetWrapper.cpp
+++ libraries/dotnet-sdk/src/CDotNetWrapper.cpp
@@ -10,49 +10,78 @@
 // license: http://creativecommons.org/licenses/by-sa/4.0/
 //--------------------------------------------------------------------
 
-#include "CDotNetWrapper.h"
+//#include "CDotNetWrapper.h"
 #include "system/CSystemBusPool"
 #include "system/CSystemBus"
 #include "system/CSystemPru"
 #include "system/CSystem"
 #include "system/Logger"
 
+#include <native/task.h>
+#define TASK_PRIO 99 /* Highest RT priority */
+#define TASK_MODE T_FPU /* Uses FPU, bound to CPU #0 */
+#define TASK_STKSZ 4096 /* Stack size (in bytes) */
+RT_TASK task_desc;
+
 #include <native/timer.h>
 
 #include "my_memory"
 
+typedef int (*callback_t)(int a, int b);
+static callback_t my_callback;
+
+int InvokeManagedCode(int a, int b) {
+	if (my_callback == NULL) {
+		printf("Managed code has not initialized this library yet");
+	}
+	return (*my_callback)(a, b);
+}
+
+void task_body(void *cookie) {
+	int i = 1;
+	for (;;) {
+		InvokeManagedCode(i++, 5);
+		rt_task_sleep(100000000);
+	}
+}
+
 
 extern "C" {
+
+void RegisterCallback(callback_t cb) {
+	my_callback = cb;
+}
+
+
+int Start() {
+	InvokeManagedCode (4, 5);
+
+	int err;
+	/* ... */
+	err = rt_task_create(&task_desc, "MyTaskName", 	TASK_STKSZ, TASK_PRIO, 	TASK_MODE);
+	if (!err)
+		rt_task_start(&task_desc, &task_body, NULL);
+
+	return err;
+}
+
+int Stop() {
+	return rt_task_delete(&task_desc);
+}
+
+}
+
+
+
 /*
-CModule* moduleCreator()
-{
-	return new CDotNetWrapper();
-}
-*/
-
-const char* moduleName() {
-	return "NetWrapper";
-}
-
-int add(int a) {
-	return a*2;
-}
-
-}
-
-
-
 CDotNetWrapper::CDotNetWrapper() :
-	CModule(1024)
-{
+		CModule(1024) {
 }
 
-CDotNetWrapper::~CDotNetWrapper()
-{
+CDotNetWrapper::~CDotNetWrapper() {
 }
 
-bool CDotNetWrapper::init(const mongo::BSONObj& initObject)
-{
+bool CDotNetWrapper::init(const mongo::BSONObj& initObject) {
 	if (!CModule::init(initObject)) {
 		return false;
 	}
@@ -65,14 +94,13 @@ bool CDotNetWrapper::init(const mongo::BSONObj& initObject)
 			pathToBin = objParam["Pru Binary"].String().c_str();
 		}
 		if (objParam.hasElement("Pru Device")) {
-			pruNumber = (int)objParam["Pru Device"].Number();
+			pruNumber = (int) objParam["Pru Device"].Number();
 		}
 	}
 	return true;
 }
 
-bool CDotNetWrapper::start()
-{
+bool CDotNetWrapper::start() {
 	CModule::start(this, &CDotNetWrapper::moduleTask);
 	return true;
 }
@@ -81,41 +109,19 @@ bool CDotNetWrapper::start()
 //  p r i v a t e   f u n c t i o n s
 //===================================================================
 
-bool CDotNetWrapper::initPwmInput()
-{
-/*
-	m_pwm[0] = (*(unsigned long *)(m_sharedMem + 0x104)/200);
-	m_pwm[1] = (*(unsigned long *)(m_sharedMem + 0x114)/200);
-	m_pwm[2] = (*(unsigned long *)(m_sharedMem + 0x124)/200);
-	m_pwm[3] = (*(unsigned long *)(m_sharedMem + 0x134)/200);
-	m_pwm[4] = (*(unsigned long *)(m_sharedMem + 0x144)/200);
-	m_pwm[5] = (*(unsigned long *)(m_sharedMem + 0x154)/200);
-	m_pwm[6] = (*(unsigned long *)(m_sharedMem + 0x164)/200);
-*/
-
+bool CDotNetWrapper::initPwmInput() {
 	return true;
 }
 
-
-void CDotNetWrapper::moduleTask()
-{
+void CDotNetWrapper::moduleTask() {
 	RTIME time = rt_timer_read();
 
-/*
-	mongo::BSONObjBuilder builder;
-	builder << "pwm0" << m_pwm[0];
-	builder << "pwm1" << m_pwm[1];
-	builder << "pwm2" << m_pwm[2];
-	builder << "pwm3" << m_pwm[3];
-	builder << "pwm4" << m_pwm[4];
-	builder << "pwm5" << m_pwm[5];
-	builder << "pwm6" << m_pwm[6];
-	sendObject(builder.obj());
-*/
-    RTIME diff = time - rt_timer_read();
-    SRTIME el = rt_timer_ticks2ns(diff);
-    uint64_t elapsed = abs(el) / 1000;
-    //printf("%5d\r",m_pwm[0]);
-    //Logger() << "PwmTASK " << elapsed;
-    //CSystem::sleep(10);
+	RTIME diff = time - rt_timer_read();
+	SRTIME el = rt_timer_ticks2ns(diff);
+	uint64_t elapsed = abs(el) / 1000;
+	//printf("%5d\r",m_pwm[0]);
+	//Logger() << "PwmTASK " << elapsed;
+	//CSystem::sleep(10);
 }
+*/
+
