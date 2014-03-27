bbb2cef54a0912df503d27e5044af7dfca5c50f0
Vova Starikh <vovastarikh@gmail.com>
2014-03-27 18:46:46 +0200

    [trivial] refactor of file

--- libraries/core/src/system/SharedPtr.h
+++ libraries/core/src/system/SharedPtr.h
@@ -35,17 +35,8 @@
 
 #pragma once
 
-#if (defined(__APPLE__) && !defined(OSX_PROJECT) && !defined(IOS_PROJECT)) || defined(__ANDROID__)
-#include <tr1/memory>
-#define CL_SharedPtr std::tr1::shared_ptr
-#define cl_dynamic_pointer_cast std::tr1::dynamic_pointer_cast
-#else
+
 #include <memory>
-#if defined(_MSC_VER) && _MSC_VER < 1600
-#define CL_SharedPtr std::tr1::shared_ptr
-#define cl_dynamic_pointer_cast std::tr1::dynamic_pointer_cast
-#else
+
 #define CL_SharedPtr std::shared_ptr
 #define cl_dynamic_pointer_cast std::dynamic_pointer_cast
-#endif
-#endif
