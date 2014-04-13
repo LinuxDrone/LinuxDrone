f33e9f6a48ac58d5451becf1c1b69dadadf6ff16
Alex Vrubel <vrubel@linuxdrone.org>
2014-04-13 20:09:26 +0400

    LD-57 Get configuration from DB

--- host-programs/c-host/src/main.c
+++ host-programs/c-host/src/main.c
@@ -1,6 +1,8 @@
 #include <dlfcn.h>
 #include <stdio.h>
+#include <bcon.h>
 #include <bson.h>
+#include <mongoc.h>
 #include "c-gy87.h"
 
 // Хранилище указателей на загруженные пускачем инстансы модулей
@@ -44,6 +46,53 @@ bson_t* get_bson_from_file() {
 	return NULL ;
 }
 
+int get_bson_from_db()
+{
+    mongoc_collection_t *collection;
+    mongoc_client_t *client;
+    mongoc_cursor_t *cursor;
+    const bson_t *item;
+    bson_error_t error;
+    bson_oid_t oid;
+    bson_t *query;
+    bson_t *doc;
+    char *str;
+    bool r;
+
+    /* get a handle to our collection */
+    client = mongoc_client_new ("mongodb://localhost:27017");
+    collection = mongoc_client_get_collection (client, "test", "configuration");
+
+    /*
+    // insert a document
+    bson_oid_init (&oid, NULL);
+    doc = BCON_NEW ("_id", BCON_OID (&oid),
+                    "hello", BCON_UTF8 ("world!"));
+    r = mongoc_collection_insert (collection, MONGOC_INSERT_NONE, doc, NULL, &error);
+    if (!r) {
+       fprintf (stderr, "%s\n", error.message);
+       return EXIT_FAILURE;
+    }
+    */
+
+    /* build a query to execute */
+    query = BCON_NEW ("name", BCON_UTF8 ("New Schema"), "version", BCON_UTF8("1"));
+
+    /* execute the query and iterate the results */
+    cursor = mongoc_collection_find (collection, MONGOC_QUERY_NONE, 0, 0, 0, query, NULL, NULL);
+    while (mongoc_cursor_next (cursor, &item)) {
+       str = bson_as_json (item, NULL);
+       printf ("%s\n", str);
+       bson_free (str);
+    }
+
+    /* release everything */
+    mongoc_cursor_destroy (cursor);
+    mongoc_collection_destroy (collection);
+    mongoc_client_destroy (client);
+    bson_destroy (query);
+    //bson_destroy (doc);
+}
 
 /**
  * @brief add_links2instance
@@ -301,6 +350,8 @@ int main(int argc, char *argv[]) {
 
     // Временное решение - грузить конфигурацию из файла, а не из базы
     bson_t* bson_configuration = get_bson_from_file();
+
+    //get_bson_from_db();
 
 	bson_iter_t iter_m;
     if (!bson_iter_init_find(&iter_m, bson_configuration, "modules")) {
