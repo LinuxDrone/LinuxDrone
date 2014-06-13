560c620eb5f4e72784213a72b687c6eb04d6914c
Andrey Gorodilov <gorodilov@mail.ru>
2014-06-13 22:38:14 +0600

    add deadband and filered
    add mode accumulate output signal
    add output = 0 in time start 10sec.

--- modules/c-pid/c-pid.c
+++ modules/c-pid/c-pid.c
@@ -39,11 +39,13 @@ void c_pid_run (module_c_pid_t *module)
     double in_err[3];
     // Выходные данные модуля
     double out[3];
-    double dT;
-
-    //enum  channel ch;
+    double dT = 0.0d;
+    float timeElapsed = 0.0f;
 
     SRTIME  timeTick, timeTickold;
+
+    memset(in_err, 0, 3*sizeof(double));
+    memset(out, 0, 3*sizeof(double));
 
     settingsLoad(pidParam, &module->params_c_pid);
 
@@ -56,26 +58,46 @@ void c_pid_run (module_c_pid_t *module)
             last_print_time = rt_timer_read();
         }
 
-        timeTick = (SRTIME)rt_timer_read();
-        dT = ((float)((rt_timer_ticks2ns(timeTick  - timeTickold)))/1000000000.0f);
-        timeTickold = timeTick;
-
         // проверим, обновились ли данные
         if(module->module_info.updated_input_properties!=0)
         {
             // есть новые данные
             input = (input_t*)module->module_info.input_data;
 
-            in_err[Roll]  = (double)input->errRoll;
-            in_err[Pitch] = (double)input->errPitch;
-            in_err[Yaw]   = (double)input->errYaw;
+            // Расчет dT
+            timeTick = (SRTIME)rt_timer_read();
+            dT = ((float)((rt_timer_ticks2ns(timeTick  - timeTickold)))/1000000000.0f);
+            timeTickold = timeTick;
+            timeElapsed += dT;
+
+            // Чтение и демпфирование входных данных
+            in_err[Roll]  = filtered(&pidParam[Roll], in_err[Roll], (double)input->errRoll);
+            in_err[Pitch]  = filtered(&pidParam[Pitch], in_err[Pitch], (double)input->errPitch);
+            in_err[Yaw]  = filtered(&pidParam[Yaw], in_err[Yaw], (double)input->errYaw);
 
             // Запускаем расчет PID для каждого канала.
+            // Модуль начинает расчитывать PID через 10 сек после запуска
             uint8_t ch;
-            for (ch = 0; ch < 3; ch++) {
-                out[ch]= calcPID(pidParam, in_err[ch], dT);
-            }
+            for (ch = 0; ch < 3 && timeElapsed > 10.0f; ch++)
+            {
+                // Проверка на выход за пределы зоны нечувствительности
+                if(in_err[ch] > pidParam[ch].dreadband || in_err[ch] < -pidParam[ch].dreadband)
+                {
+                    // Расчет PID
+                        double tmp = calcPID(&pidParam[ch], in_err[ch], dT);
 
+                    // Для режима с накоплением расчитанных воздействий PID сигнала
+                    // Нужен для сохранения величины выходного сигнала после компенсации рассогласования по входу
+                    // TODO сделать отдельный модуль для этой цели.
+                    if(pidParam[ch].limitAccum > 0.00001d)
+                    {
+                        out[ch] += tmp * dT;
+                        if(out[ch] >  pidParam[ch].limitAccum) out[ch] = pidParam[ch].limitAccum;
+                        if(out[ch] < -pidParam[ch].limitAccum) out[ch] = -pidParam[ch].limitAccum;
+
+                    } else out[ch] = tmp; // Обычный выход с PID
+                }
+            }
 
             outPID_t* outpid;
             checkout_outPID(module, &outpid);
@@ -94,7 +116,6 @@ void c_pid_run (module_c_pid_t *module)
         //module->module_info.refresh_input_mask = errRoll | errPitch | errYaw;
 
 
-        //if(printEnable) printf("module->params_c_pid.PitchKp = %f\n",module->params_c_pid.RollKp);//pidParam[Roll].Kp);
         //if(printEnable) print_params_c_pid(&module->params_c_pid);
         printEnable = false;
     }
@@ -112,5 +133,15 @@ void settingsLoad(pidParams_t *pidParam, params_c_pid_t *param)
   setPID(&pidParam[Roll],   param->RollKp, param->RollKi,param->RollKd,  param->RollLimit);
   setPID(&pidParam[Pitch],  param->PitchKp, param->PitchKi,param->PitchKd,  param->PitchLimit);
   setPID(&pidParam[Yaw],    param->YawKp, param->YawKi,param->YawKd,  param->YawLimit);
+
+  pidParam[Roll].filteredAlfa   = param->RollFilteredAlfa;
+  pidParam[Roll].limitAccum     = param->RollLimitAccum;
+  pidParam[Roll].dreadband      = param->RollDeadband;
+  pidParam[Pitch].filteredAlfa  = param->PitchFilteredAlfa;
+  pidParam[Pitch].limitAccum    = param->PitchLimitAccum;
+  pidParam[Pitch].dreadband     = param->PitchDeadband;
+  pidParam[Yaw].filteredAlfa    = param->YawFilteredAlfa;
+  pidParam[Yaw].limitAccum      = param->YawLimitAccum;
+  pidParam[Yaw].dreadband       = param->YawDeadband;
 }
 
