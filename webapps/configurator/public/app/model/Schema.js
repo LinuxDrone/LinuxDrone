e7bc6c7047f88cfaa5497a9390cba505c4931092
Alex Vrubel <vrubel@linuxdrone.org>
2014-06-23 21:32:47 +0400

    Забыл

---
+++ webapps/configurator/public/app/model/Schema.js
@@ -0,0 +1,7 @@
+/**
+ * Created by vrubel on 22.06.14.
+ */
+Ext.define('RtConfigurator.model.Schema', {
+    extend: 'RtConfigurator.model.Base'
+
+});
---
+++ webapps/configurator/public/app/store/StoreSchemas.js
@@ -0,0 +1,16 @@
+/**
+ * Created by vrubel on 22.06.14.
+ */
+Ext.define('RtConfigurator.store.StoreSchemas', {
+    extend: 'Ext.data.JsonStore',
+    autoLoad: true,
+    storeId: 'schemasStore',
+
+    model: 'RtConfigurator.model.Schema',
+
+    proxy: {
+        type: 'rest',
+        url: '/getconfigs'
+    }
+});
+
