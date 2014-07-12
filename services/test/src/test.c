304695a0c348628441634076c167f4dad4a99186
Andrey Gorodilov <gorodilov@mail.ru>
2014-07-12 19:34:34 +0600

    check this simple codetest

---
+++ services/test/src/test.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+
+
+#include "../include/test.h"
+
+
+int main()
+{
+    //mlockall(MCL_CURRENT|MCL_FUTURE);
+
+    //int n = 0;
+    //int use_ssl = 0;
+    //int opts = 0;
+    //char interface_name[128] = "";
+    //const char *iface = NULL;
+    //unsigned int oldus = 0;
+
+    printf("Hello world!\n");
+
+    return 0;
+}
