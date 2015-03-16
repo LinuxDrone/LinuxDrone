26af7e0c036dd69ac93c7cba05d8fce659bc4444
Alex Vrubel <vrubel@linuxdrone.org>
2015-03-16 17:56:15 +0300

    Generate VC project for module

--- modules/GenerateModuleTemplate.js
+++ modules/GenerateModuleTemplate.js
@@ -1,3 +1,15 @@
+function GenerateGUID() {
+    var d, guid;
+    d = new Date().getTime();
+    guid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+        var r;
+        r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === "x" ? r : r & 0x7 | 0x8).toString(16);
+    });
+    return guid;
+}
+
 function CreateCMakeLists(module_name) {
     var r = "";
     r += "#--------------------------------------------------------------------\n";
@@ -355,7 +367,7 @@ function CreateModuleCFile(obj_def) {
             for (var key in out.Schema.properties) {
                 if (out.Schema.properties[key].type == "const char*") {
                     r += "        char buffer_" + key + " [32];\n";
-                    r += "        snprintf(buffer_" + key + ", 32, \"data: %d\", cycle);\n";
+                    r += "        printf(buffer_" + key + ", 32, \"data: %d\", cycle);\n";
                     r += "        obj" + outName + "->" + key + " = buffer_" + key + ";\n";
                 } else {
                     if ('inputShema' in obj_def) {
@@ -410,6 +422,186 @@ function CreateModuleCFile(obj_def) {
     return r;
 }
 
+function CreateVCProject(obj_def){
+    var module_name = obj_def.name;
+    var r = "";
+    r += "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
+    r += "<Project DefaultTargets=\"Build\" ToolsVersion=\"12.0\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">\n";
+    r += "    <ItemGroup Label=\"ProjectConfigurations\">\n";
+    r += "    <ProjectConfiguration Include=\"Debug|Win32\">\n";
+    r += "    <Configuration>Debug</Configuration>\n";
+    r += "    <Platform>Win32</Platform>\n";
+    r += "    </ProjectConfiguration>\n";
+    r += "    <ProjectConfiguration Include=\"Debug|x64\">\n";
+    r += "    <Configuration>Debug</Configuration>\n";
+    r += "    <Platform>x64</Platform>\n";
+    r += "    </ProjectConfiguration>\n";
+    r += "    <ProjectConfiguration Include=\"Release|Win32\">\n";
+    r += "    <Configuration>Release</Configuration>\n";
+    r += "    <Platform>Win32</Platform>\n";
+    r += "    </ProjectConfiguration>\n";
+    r += "    <ProjectConfiguration Include=\"Release|x64\">\n";
+    r += "    <Configuration>Release</Configuration>\n";
+    r += "    <Platform>x64</Platform>\n";
+    r += "    </ProjectConfiguration>\n";
+    r += "    </ItemGroup>\n";
+    r += "    <PropertyGroup Label=\"Globals\">\n";
+    r += "    <ProjectGuid>{" + GenerateGUID() + "}</ProjectGuid>\n";
+    r += "    <Keyword>Win32Proj</Keyword>\n";
+    r += "    <RootNamespace>" + module_name.replace(/-/g, "") + "</RootNamespace>\n";
+    r += "    </PropertyGroup>\n";
+    r += "    <Import Project=\"$(VCTargetsPath)\\Microsoft.Cpp.Default.props\" />\n";
+    r += "    <PropertyGroup Condition=\"'$(Configuration)|$(Platform)'=='Debug|Win32'\" Label=\"Configuration\">\n";
+    r += "    <ConfigurationType>Application</ConfigurationType>\n";
+    r += "    <UseDebugLibraries>true</UseDebugLibraries>\n";
+    r += "    <PlatformToolset>v120</PlatformToolset>\n";
+    r += "    <CharacterSet>Unicode</CharacterSet>\n";
+    r += "    </PropertyGroup>\n";
+    r += "    <PropertyGroup Condition=\"'$(Configuration)|$(Platform)'=='Debug|x64'\" Label=\"Configuration\">\n";
+    r += "    <ConfigurationType>Application</ConfigurationType>\n";
+    r += "    <UseDebugLibraries>true</UseDebugLibraries>\n";
+    r += "    <PlatformToolset>v120</PlatformToolset>\n";
+    r += "    <CharacterSet>Unicode</CharacterSet>\n";
+    r += "    </PropertyGroup>\n";
+    r += "    <PropertyGroup Condition=\"'$(Configuration)|$(Platform)'=='Release|Win32'\" Label=\"Configuration\">\n";
+    r += "    <ConfigurationType>Application</ConfigurationType>\n";
+    r += "    <UseDebugLibraries>false</UseDebugLibraries>\n";
+    r += "    <PlatformToolset>v120</PlatformToolset>\n";
+    r += "    <WholeProgramOptimization>true</WholeProgramOptimization>\n";
+    r += "    <CharacterSet>Unicode</CharacterSet>\n";
+    r += "    </PropertyGroup>\n";
+    r += "    <PropertyGroup Condition=\"'$(Configuration)|$(Platform)'=='Release|x64'\" Label=\"Configuration\">\n";
+    r += "    <ConfigurationType>Application</ConfigurationType>\n";
+    r += "    <UseDebugLibraries>false</UseDebugLibraries>\n";
+    r += "    <PlatformToolset>v120</PlatformToolset>\n";
+    r += "    <WholeProgramOptimization>true</WholeProgramOptimization>\n";
+    r += "    <CharacterSet>Unicode</CharacterSet>\n";
+    r += "    </PropertyGroup>\n";
+    r += "    <Import Project=\"$(VCTargetsPath)\\Microsoft.Cpp.props\" />\n";
+    r += "    <ImportGroup Label=\"ExtensionSettings\">\n";
+    r += "    </ImportGroup>\n";
+    r += "    <ImportGroup Label=\"PropertySheets\" Condition=\"'$(Configuration)|$(Platform)'=='Debug|Win32'\">\n";
+    r += "    <Import Project=\"$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props\" Condition=\"exists('$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props')\" Label=\"LocalAppDataPlatform\" />\n";
+    r += "    </ImportGroup>\n";
+    r += "    <ImportGroup Condition=\"'$(Configuration)|$(Platform)'=='Debug|x64'\" Label=\"PropertySheets\">\n";
+    r += "    <Import Project=\"$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props\" Condition=\"exists('$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props')\" Label=\"LocalAppDataPlatform\" />\n";
+    r += "    </ImportGroup>\n";
+    r += "    <ImportGroup Label=\"PropertySheets\" Condition=\"'$(Configuration)|$(Platform)'=='Release|Win32'\">\n";
+    r += "    <Import Project=\"$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props\" Condition=\"exists('$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props')\" Label=\"LocalAppDataPlatform\" />\n";
+    r += "    </ImportGroup>\n";
+    r += "    <ImportGroup Condition=\"'$(Configuration)|$(Platform)'=='Release|x64'\" Label=\"PropertySheets\">\n";
+    r += "    <Import Project=\"$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props\" Condition=\"exists('$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props')\" Label=\"LocalAppDataPlatform\" />\n";
+    r += "    </ImportGroup>\n";
+    r += "    <PropertyGroup Label=\"UserMacros\" />\n";
+    r += "    <PropertyGroup Condition=\"'$(Configuration)|$(Platform)'=='Debug|Win32'\">\n";
+    r += "    <LinkIncremental>true</LinkIncremental>\n";
+    r += "    <IncludePath>$(SolutionDir)tools\\apr-1.5.1\\include;$(SolutionDir)tools\\libbson-win\\include\\libbson-1.0;$(SolutionDir)libraries\\sdk\\include;$(IncludePath)</IncludePath>\n";
+    r += "    <LibraryPath>$(SolutionDir)tools\\apr-1.5.1\x64\\Release;$(LibraryPath)</LibraryPath>\n";
+    r += "    </PropertyGroup>\n";
+    r += "    <PropertyGroup Condition=\"'$(Configuration)|$(Platform)'=='Debug|x64'\">\n";
+    r += "    <LinkIncremental>true</LinkIncremental>\n";
+    r += "    <IncludePath>$(SolutionDir)tools\\apr-1.5.1\\include;$(SolutionDir)tools\\libbson-win\\include\\libbson-1.0;$(SolutionDir)libraries\\sdk\\include;$(IncludePath)</IncludePath>\n";
+    r += "    <LibraryPath>$(SolutionDir)tools\\apr-1.5.1\\x64\\Release;$(LibraryPath)</LibraryPath>\n";
+    r += "    <PreBuildEventUseInBuild>true</PreBuildEventUseInBuild>\n";
+    r += "    <TargetName>$(ProjectName).mod</TargetName>\n";
+    r += "    </PropertyGroup>\n";
+    r += "    <PropertyGroup Condition=\"'$(Configuration)|$(Platform)'=='Release|Win32'\">\n";
+    r += "    <LinkIncremental>false</LinkIncremental>\n";
+    r += "    </PropertyGroup>\n";
+    r += "    <PropertyGroup Condition=\"'$(Configuration)|$(Platform)'=='Release|x64'\">\n";
+    r += "    <LinkIncremental>false</LinkIncremental>\n";
+    r += "    </PropertyGroup>\n";
+    r += "    <ItemDefinitionGroup Condition=\"'$(Configuration)|$(Platform)'=='Debug|Win32'\">\n";
+    r += "    <ClCompile>\n";
+    r += "    <PrecompiledHeader>\n";
+    r += "    </PrecompiledHeader>\n";
+    r += "    <WarningLevel>Level3</WarningLevel>\n";
+    r += "    <Optimization>Disabled</Optimization>\n";
+    r += "    <PreprocessorDefinitions>WIN32;_DEBUG;_CONSOLE;_LIB;%(PreprocessorDefinitions)</PreprocessorDefinitions>\n";
+    r += "    <SDLCheck>true</SDLCheck>\n";
+    r += "    </ClCompile>\n";
+    r += "    <Link>\n";
+    r += "    <SubSystem>Console</SubSystem>\n";
+    r += "    <GenerateDebugInformation>true</GenerateDebugInformation>\n";
+    r += "    </Link>\n";
+    r += "    <PreBuildEvent>\n";
+    r += "    <Command>node $(SolutionDir)libraries\\sdk\\ModuleGenerator.js $(ProjectDir)" + module_name + ".def.json $(ProjectDir) MSVC</Command>\n";
+    r += "    </PreBuildEvent>\n";
+    r += "    </ItemDefinitionGroup>\n";
+    r += "    <ItemDefinitionGroup Condition=\"'$(Configuration)|$(Platform)'=='Debug|x64'\">\n";
+    r += "    <ClCompile>\n";
+    r += "    <PrecompiledHeader>\n";
+    r += "    </PrecompiledHeader>\n";
+    r += "    <WarningLevel>Level3</WarningLevel>\n";
+    r += "    <Optimization>Disabled</Optimization>\n";
+    r += "    <PreprocessorDefinitions>WIN32;_DEBUG;_CONSOLE;_LIB;%(PreprocessorDefinitions)</PreprocessorDefinitions>\n";
+    r += "    <SDLCheck>true</SDLCheck>\n";
+    r += "    </ClCompile>\n";
+    r += "    <Link>\n";
+    r += "    <SubSystem>Console</SubSystem>\n";
+    r += "    <GenerateDebugInformation>true</GenerateDebugInformation>\n";
+    r += "    <AdditionalDependencies>libapr-1.lib;bson-1.0.lib;%(AdditionalDependencies)</AdditionalDependencies>\n";
+    r += "    <AdditionalLibraryDirectories>$(SolutionDir)tools\\libbson\\Debug;%(AdditionalLibraryDirectories)</AdditionalLibraryDirectories>\n";
+    r += "    </Link>\n";
+    r += "    <PreBuildEvent>\n";
+    r += "    <Command>node $(SolutionDir)libraries\\sdk\\ModuleGenerator.js $(ProjectDir)" + module_name + ".def.json $(ProjectDir) MSVC</Command>\n";
+    r += "    </PreBuildEvent>\n";
+    r += "    </ItemDefinitionGroup>\n";
+    r += "    <ItemDefinitionGroup Condition=\"'$(Configuration)|$(Platform)'=='Release|Win32'\">\n";
+    r += "    <ClCompile>\n";
+    r += "    <WarningLevel>Level3</WarningLevel>\n";
+    r += "    <PrecompiledHeader>\n";
+    r += "    </PrecompiledHeader>\n";
+    r += "    <Optimization>MaxSpeed</Optimization>\n";
+    r += "    <FunctionLevelLinking>true</FunctionLevelLinking>\n";
+    r += "    <IntrinsicFunctions>true</IntrinsicFunctions>\n";
+    r += "    <PreprocessorDefinitions>WIN32;NDEBUG;_CONSOLE;_LIB;%(PreprocessorDefinitions)</PreprocessorDefinitions>\n";
+    r += "    <SDLCheck>true</SDLCheck>\n";
+    r += "    </ClCompile>\n";
+    r += "    <Link>\n";
+    r += "    <SubSystem>Console</SubSystem>\n";
+    r += "    <GenerateDebugInformation>true</GenerateDebugInformation>\n";
+    r += "    <EnableCOMDATFolding>true</EnableCOMDATFolding>\n";
+    r += "    <OptimizeReferences>true</OptimizeReferences>\n";
+    r += "    </Link>\n";
+    r += "    </ItemDefinitionGroup>\n";
+    r += "    <ItemDefinitionGroup Condition=\"'$(Configuration)|$(Platform)'=='Release|x64'\">\n";
+    r += "    <ClCompile>\n";
+    r += "    <WarningLevel>Level3</WarningLevel>\n";
+    r += "    <PrecompiledHeader>\n";
+    r += "    </PrecompiledHeader>\n";
+    r += "    <Optimization>MaxSpeed</Optimization>\n";
+    r += "    <FunctionLevelLinking>true</FunctionLevelLinking>\n";
+    r += "    <IntrinsicFunctions>true</IntrinsicFunctions>\n";
+    r += "    <PreprocessorDefinitions>WIN32;NDEBUG;_CONSOLE;_LIB;%(PreprocessorDefinitions)</PreprocessorDefinitions>\n";
+    r += "    <SDLCheck>true</SDLCheck>\n";
+    r += "    </ClCompile>\n";
+    r += "    <Link>\n";
+    r += "    <SubSystem>Console</SubSystem>\n";
+    r += "    <GenerateDebugInformation>true</GenerateDebugInformation>\n";
+    r += "    <EnableCOMDATFolding>true</EnableCOMDATFolding>\n";
+    r += "    <OptimizeReferences>true</OptimizeReferences>\n";
+    r += "    </Link>\n";
+    r += "    </ItemDefinitionGroup>\n";
+    r += "    <ItemGroup>\n";
+    r += "    <ClCompile Include=\"" + module_name + ".c\" />\n";
+    r += "    <ClCompile Include=\"" + module_name + ".helper.c\" />\n";
+    r += "    </ItemGroup>\n";
+    r += "    <ItemGroup>\n";
+    r += "    <ClInclude Include=\"" + module_name + ".helper.h\" />\n";
+    r += "    </ItemGroup>\n";
+    r += "    <ItemGroup>\n";
+    r += "    <ProjectReference Include=\"..\\..\\libraries\\sdk\\sdk.vcxproj\">\n";
+    r += "    <Project>{c239d93d-f3d7-49f8-b198-4d9965f96af9}</Project>\n";
+    r += "    </ProjectReference>\n";
+    r += "    </ItemGroup>\n";
+    r += "    <Import Project=\"$(VCTargetsPath)\\Microsoft.Cpp.targets\" />\n";
+    r += "    <ImportGroup Label=\"ExtensionTargets\">\n";
+    r += "    </ImportGroup>\n";
+    r += "    </Project>\n";
+
+    return r;
+}
 
 function main() {
     if (process.argv.length < 3) {
@@ -445,7 +637,11 @@ function main() {
     fs.writeFile(module_name + "/" + module_name + '.c', CreateModuleCFile(obj_def), function (err) {
         if (err) return console.log(err);
     });
-}
 
+
+    fs.writeFile(module_name + "/" + module_name + '.vcxproj', CreateVCProject(obj_def), function (err) {
+        if (err) return console.log(err);
+    });
+}
 
 main();
