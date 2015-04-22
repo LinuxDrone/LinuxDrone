7a0aa3d6d2da923d13dfb5ebb576bba3802a2d5d
Alex Vrubel <vrubel@linuxdrone.org>
2015-04-22 19:27:55 +0300

    saveconfig and getconfig functions for file store

--- webapps/configurator/server/useFiles.js
+++ webapps/configurator/server/useFiles.js
@@ -1,4 +1,6 @@
+var CFG_FOLDER = "../../cfg";
 
+var fs = require('fs');
 
 var hostStatus = {
     status: 'stopped', // running
@@ -17,3 +19,56 @@ exports.gethoststatus = function (req, res) {
     }
     res.end();
 };
+
+exports.getconfigs = function (req, res) {
+    fs.readdir(CFG_FOLDER, function (err, list) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        var cfgFiles = new Array();
+        list.forEach(function (file) {
+            // для начала проверим, что расширение файла .json
+            var ar = file.split('.');
+
+            if (ar[ar.length-1] == "json") {
+                var cfg = require(CFG_FOLDER + '/' + file);
+                cfgFiles.push(cfg);
+            }
+        });
+
+        if (req.body.callback) {
+            res.writeHead(200, {"Content-Type": "application/javascript"});
+            res.write(req.body.callback + '(' + JSON.stringify(cfgFiles) + ')');
+        } else {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.write(JSON.stringify(cfgFiles));
+        }
+        res.end();
+    });
+};
+
+
+exports.saveconfig = function (req, res) {
+    var record = JSON.parse(req.body.records);
+
+    var cfgFileName = CFG_FOLDER + "/" + record.name + "." + record.version + ".json";
+    fs.writeFile(cfgFileName, req.body.records, function(err) {
+        if(err) {
+            res.send({"success": false, "message": err});
+            return console.log(err);
+        }
+
+        if (req.body.callback) {
+            res.writeHead(200, {"Content-Type": "application/javascript"});
+            res.write(req.body.callback + '({"success": true})');
+        } else {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.write('{"success": true}');
+        }
+
+        console.log("The file \""+cfgFileName+"\" was saved!");
+    });
+}
+
