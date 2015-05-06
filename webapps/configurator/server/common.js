b478ceca9ae2a7eb0c6adc76c85ab4068ababa90
Alex Vrubel <vrubel@linuxdrone.org>
2015-05-06 16:04:54 +0300

    Convert graph to instances definitions

--- webapps/configurator/server/common.js
+++ webapps/configurator/server/common.js
@@ -3,9 +3,154 @@ exports.CFG_FOLDER = "../../cfg";
 //var BIN_FOLDER = "../../bin"; // Linux BBB
 var BIN_FOLDER = "../../../x64/Debug"; // Windows
 
-
+var commonModuleParams = require('../client/ModulesCommonParams.def.js');
+var _ = require('underscore');
 var fs = require('fs');
 var exec = require('child_process').exec;
+
+
+// Приведение значения к типу
+function CastValue2Type(value, type) {
+    switch (type) {
+        case "char":
+        case "short":
+        case "int":
+        case "long":
+        case "long long":
+        case "float":
+        case "double":
+            value = Number(value);
+            break;
+
+        case "const char*":
+            break;
+
+        case "bool":
+            if (_.isString(value) && value === "false") {
+                value = false;
+            }
+            else {
+                value = Boolean(value);
+            }
+            break;
+
+        default:
+            console.log("CastValue2Type Unknown type: " + type);
+            break;
+    }
+
+    return value;
+}
+
+
+// Возвращает название инстанса по идентификатору
+function GetInstanceName(graph, id) {
+    var res;
+    graph.cells.forEach(function (cell) {
+        if (cell.id == id) {
+            res = cell.attrs[".label"].text;
+        }
+    });
+    return res;
+}
+
+
+// Выгреюает информацию о инстансе из визуальной конфигурации и раскладывает по полочкам
+function ConvertVisualCell(graph, arModules, arLinks, cell, modulesParams, metaModules) {
+    if (cell.type == "devs.Model") {
+
+        var module = {
+            "name": cell.moduleType,
+            /* Название instance модуля*/
+            "instance": cell.attrs[".label"].text
+        };
+
+        var metaModule = _.find(metaModules, function (meta) {
+            return meta.name == cell.moduleType;
+        });
+
+        // Перенос общих (определенных для всех типов модулей) параметров
+        if (modulesParams && modulesParams[module.instance].common) { //&& modulesParams[module.instance]
+            var commonParams = modulesParams[module.instance].common;
+            Object.keys(commonParams).forEach(function (paramName) {
+                var metaParam = _.find(commonModuleParams.commonModuleParamsDefinition, function (meta) {
+                    return meta.name == paramName;
+                });
+                if (!metaParam) {
+                    console.log("In instance " + module.instance + ". Not found metadata for parameter '" + paramName + "'  in common definition for modules ");
+                }
+                var typedValue = CastValue2Type(commonParams[paramName], metaParam.type);
+                if (typedValue === undefined) {
+                    console.log("Unknown type '" + metaParam.type + "' in metadata for parameter '" + paramName + "' in common definition for modules");
+                }
+                module[paramName] = typedValue;
+            });
+        }
+
+        // Перенос специфических для модуля параметров
+        var specificParams = modulesParams[module.instance].specific;
+        if (specificParams) {
+            module.params = {};
+            Object.keys(specificParams).forEach(function (paramName) {
+                var metaParam = _.find(metaModule.paramsDefinitions, function (meta) {
+                    return meta.name == paramName;
+                });
+                if (!metaParam) {
+                    console.log("Not found metadata for parameter '" + paramName + "' for module '" + cell.moduleType + "'");
+                }
+                else {
+                    var typedValue = CastValue2Type(specificParams[paramName], metaParam.type);
+                    if (typedValue === undefined) {
+                        console.log("Unknown type '" + metaParam.type + "' in metadata for parameter '" + paramName + "' for module " + cell.moduleType + "'");
+                    }
+                    module.params[paramName] = typedValue;
+                }
+            });
+        }
+
+        // Перенос конфигурации сложных модулей
+        if (cell.blocksJSON) {
+            module.blocksConfig = {"blocks": new Array(), "links": new Array()};
+
+            cell.blocksJSON.cells.forEach(function (blockInstance) {
+                var blockParams = modulesParams[module.instance].blocksConfig;
+                var metaBlocks = metaModule.subSchema.blocksDefinitions;
+
+                ConvertVisualCell(cell.blocksJSON, module.blocksConfig.blocks, module.blocksConfig, blockInstance, blockParams, metaBlocks);
+            });
+        }
+
+        arModules.push(module);
+    }
+
+    if (cell.type == "link") {
+        arLinks.links.push({
+            "type": cell.mode,
+            "portType": cell.portType, // Тип значения порта
+            "outInst": GetInstanceName(graph, cell.source.id),
+            "inInst": GetInstanceName(graph, cell.target.id),
+            "outPin": cell.source.port,
+            "inPin": cell.target.port,
+            "nameOutGroup": cell.nameOutGroup
+        });
+    }
+}
+
+// Конвертирует визуальное представление графа в конфигурацию модулей принятую в linuxdrone
+function ConvertGraph2Configuration(graph, modulesParams, metaModules) {
+    var config = {
+        "type": "configuration",
+        "modules": new Array(),
+        "links": new Array()
+    };
+
+    graph.cells.forEach(function (cell) {
+        ConvertVisualCell(graph, config.modules, config, cell, modulesParams, metaModules);
+    });
+
+    //console.log(config);
+    return config;
+}
 
 
 exports.requireUncached = function(module){
@@ -140,14 +285,21 @@ exports.runhosts = function (req, res) {
                 if (schema.name === req.body.name && schema.version === req.body.version) {
                     console.log('Нашлась схема ' + schema.name + " " + schema.version);
 
-                    if (req.body.callback) {
-                        res.writeHead(200, {"Content-Type": "application/javascript"});
-                        res.write(req.body.callback + '(' + JSON.stringify({"success": true}) + ')');
-                    } else {
-                        res.writeHead(200, {"Content-Type": "application/json"});
-                        res.write(JSON.stringify({"success": true}));
-                    }
-                    res.end();
+                    // Теперь надо вытащить из схемы все модули, сформировать для каждого из них параметры командной строки запустить каждый в отдельном процессе.
+                    exports.MetaOfModules.get(function (metaModules) {
+                        // Получаем описания модулей и вызываем функцию, передавай ей граф объектов из графической схемы и настроечные параметры модулей.
+                        var configuration = ConvertGraph2Configuration(JSON.parse(schema.jsonGraph), schema.modulesParams, metaModules);
+
+
+                        if (req.body.callback) {
+                            res.writeHead(200, {"Content-Type": "application/javascript"});
+                            res.write(req.body.callback + '(' + JSON.stringify({"success": true}) + ')');
+                        } else {
+                            res.writeHead(200, {"Content-Type": "application/json"});
+                            res.write(JSON.stringify({"success": true}));
+                        }
+                        res.end();
+                    });
                 }
             }
         });
